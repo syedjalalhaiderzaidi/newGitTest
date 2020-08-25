4b5562ea6a15035a7e53842698e7bfa36bafb84f
Syed Jalal Haider Zaidi <jalal.haider@nextbridge.org>
2020-08-25 02:50:56 -0700

    multilevel_inheritance file updated

--- multilevel_inheritance.py
+++ multilevel_inheritance.py
@@ -1,3 +1,4 @@
+"""
 class A: 
     def __init__(self): 
         print('HEY !!!!!! I am initialised(Class A)') 
@@ -35,3 +36,54 @@ if __name__ == '__main__':
     # calling the function func_print() from class C 
     # which inherits both A and B classes 
     grandChild.func_print(10) 
+"""
+
+class Family:
+    def __init__(self): 
+        print('HEY !!!!!! I am initialised(Class Family)') 
+    def show_family(self):
+        print("This is our family:")
+ 
+ 
+# Father class inherited from Family
+class Father(Family):
+    fathername = ""
+    def __init__(self): 
+        print('HEY !!!!!!I am initialised(Class Father)') 
+        super().__init__()
+    
+    def show_father(self):
+        print(self.fathername)
+ 
+ 
+# Mother class inherited from Family
+class Mother(Family):
+
+    mothername = ""
+    def __init__(self): 
+        print('HEY !!!!!!I am initialised(Class Mother)') 
+        super().__init__()
+ 
+    def show_mother(self):
+        print(self.mothername)
+ 
+ 
+# Son class inherited from Father and Mother classes
+class Son(Father, Mother):
+    def __init__(self): 
+        print('HEY !!!!!!I am initialised(Class Son)') 
+        super().__init__()
+  
+    def show_parent(self):
+        print("Father :", self.fathername)
+        print("Mother :", self.mothername)
+ 
+ 
+s1 = Son()  # Object of Son class
+s1.fathername = "Mark"
+s1.mothername = "Sonia"
+s1.show_family()
+s1.show_father()
+s1.show_mother()
+s1.show_parent()
+ 
